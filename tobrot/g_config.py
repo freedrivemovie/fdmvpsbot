80c607b335d000583d97a1d51cc9e94ea6d21fa2
freedrivemovie <96814706+freedrivemovie@users.noreply.github.com>
2022-10-27 15:05:02 +0600

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -2,12 +2,12 @@ from tobrot.sample_config import Config
 #Fill your all data, read readme for reference
 
 class Config(Config):
-    TG_BOT_TOKEN= "1268fill---your----data"
-    APP_ID = 1373938
-    API_HASH = "fill--your--data"
-    OWNER_ID = 12537936
-    AUTH_CHANNEL = [-10082786282972]
-    DESTINATION_FOLDER = "TorrentLeech-Gdrive" #Name of your folder read readme(not id of the folder)
-    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.fill---your---data----7gGcxnYr71RBuoMVlzhAM_-fil your davxrOpIuvZ-S5lo4YzhYqP:"Bearer","refresh_token":"1//0gnh0j3fUzRITCgYIARAAGBASNwF-L9IrbbYT1QkJg6UCEo3AksHDp6DqUBrhcTBIwUWtlgmbnBQPZKvSAEOTl098VTt8zEojRnY","expiry":"2fill--your--data-07-13T10:4fillyourdata758376Z"}"""
+    TG_BOT_TOKEN= "5492691108:AAFhFgRYgEkYu8NRf-SN93BNweWCGSXsGXQ"
+    APP_ID = 12899143
+    API_HASH = "0091bdc55aacee12b5b0ab73546df90c"
+    OWNER_ID = 1963848011
+    AUTH_CHANNEL = [-751911777]
+    DESTINATION_FOLDER = "FDM-Shared Drive" #Name of your folder read readme(not id of the folder)
+    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.a0Aa4xrXOpv2ZrPjH53RwQ-6b4o1_QEVXX5Wj49fPq7xCwivqJ5Xdm0AHIsHNCIVqfy916lf_csZdwJ359i7ve4kqu9rQ-DOG8v5tVjLYGaDSMCOhTw_E9DE6IViJ_E8bO9CLUsxW-iOjOwF7JX3R1NbM9F3bkaCgYKATASARISFQEjDvL9eWIR47dv3a9LQWlsl4xV8g0163","token_type":"Bearer","refresh_token":"1//0gkDlidGyf_2tCgYIARAAGBASNwF-L9IrFrwQRx6IFnnQwKx8DgL7MDe9nobgxjK39VR1ZaI1kC-0BRI0lUsr4cEQp2c7xS1HrWo","expiry":"2022-10-27T15:43:02.1702463+06:00"}"""
     #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
     #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
